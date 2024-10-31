e483e717743c9e72a8838194e678d6c97928aa22
julian <julian@cypherstack.com>
2024-10-31 08:19:02 -0600

    refactor and add hash check to ffi bindings gen script

--- tools/ffi_bindings/generate_bindings.sh
+++ tools/ffi_bindings/generate_bindings.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+source ../env.sh
 set -x -e
 
-CURRENT_DIR=$(pwd)
-
-if [[ ! -d "${CURRENT_DIR}/../../build/monero_c" ]];
+if [[ ! -d "${MONERO_C_DIR}" ]];
 then
-  echo "Missing source needed for bindings generation"
+  echo "Missing source needed for bindings generation!"
   exit 1
 fi
 
-pushd "../../cs_monero"
-dart run ffigen --config "${CURRENT_DIR}/monero.yaml"
-dart run ffigen --config "${CURRENT_DIR}/wownero.yaml"
+pushd "${MONERO_C_DIR}"
+CHECKED_OUT_HASH="$(git rev-parse HEAD)"
+if [ "${CHECKED_OUT_HASH}" != "${MONERO_C_HASH}" ];
+then
+  echo "Current monero_c hash does not match expected commit!"
+  exit 1
+fi
+popd
+
+pushd "${PROJECT_DIR}/cs_monero"
+dart run ffigen --config "${TOOLS_DIR}/ffi_bindings/monero.yaml"
+dart run ffigen --config "${TOOLS_DIR}/ffi_bindings/wownero.yaml"
 popd
 
 echo "FFI bindings generation completed"
