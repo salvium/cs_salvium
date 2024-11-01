0e5ee14486c82e5e87dd5aa184f73374a03fa184
julian <julian@cypherstack.com>
2024-10-31 22:22:09 -0600

    published state

--- cs_monero_flutter_libs_android/lib/cs_monero_flutter_libs_android.dart
+++ cs_monero_flutter_libs_android/lib/cs_monero_flutter_libs_android.dart
@@ -1,3 +1,6 @@
+import 'package:cs_monero_flutter_libs_android_arm64_v8a/cs_monero_flutter_libs_android_arm64_v8a.dart';
+import 'package:cs_monero_flutter_libs_android_armeabi_v7a/cs_monero_flutter_libs_android_armeabi_v7a.dart';
+import 'package:cs_monero_flutter_libs_android_x86_64/cs_monero_flutter_libs_android_x86_64.dart';
 import 'package:cs_monero_flutter_libs_platform_interface/cs_monero_flutter_libs_platform_interface.dart';
 import 'package:flutter/services.dart';
 
@@ -11,6 +14,13 @@ class CsMoneroFlutterLibsAndroid extends CsMoneroFlutterLibsPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
+    // make calls so flutter doesn't tree shake
+    await Future.wait([
+      CsMoneroFlutterLibsAndroidArm64V8a().getPlatformVersion(),
+      CsMoneroFlutterLibsAndroidArmeabiV7a().getPlatformVersion(),
+      CsMoneroFlutterLibsAndroidX8664().getPlatformVersion(),
+    ]);
+
     final version = await _channel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
--- cs_monero_flutter_libs_platform_interface/CHANGELOG.md
+++ cs_monero_flutter_libs_platform_interface/CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1-dev.0
 
 * initial testing release
+
+* ## 0.0.1-dev.1
+
+* License
