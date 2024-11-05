72dc3b114b9c767554d971bec6f44beee05f2463
julian <julian@cypherstack.com>
2024-11-04 19:33:20 -0600

    test: add recipient model tests and docs

--- cs_monero/lib/src/models/recipient.dart
+++ cs_monero/lib/src/models/recipient.dart
@@ -1,6 +1,18 @@
+/// Represents a recipient in a transaction, including their address and the
+/// amount to be transferred.
 class Recipient {
-  Recipient({required this.address, required this.amount});
+  /// Creates a [Recipient] with the given [address] and [amount].
+  ///
+  /// Throws an assertion error if [address] is empty or [amount] is negative.
+  Recipient({
+    required this.address,
+    required this.amount,
+  }) : assert(address.isNotEmpty && !amount.isNegative);
 
+  /// The address of the recipient where the funds will be sent.
   final String address;
+
+  /// The amount of funds to be transferred to the recipient,
+  /// represented in atomic units.
   final BigInt amount;
 }
