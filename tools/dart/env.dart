eef5e37f6ccf0ea61538c3e68ade731ace117283
sneurlax <sneurlax@gmail.com>
2024-12-05 14:30:32 -0600

    feat: update monero_c hash to point to latest monero_c#output-details
    as of this commit

--- tools/dart/env.dart
+++ tools/dart/env.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 const kMoneroCRepo = "https://github.com/MrCyjaneK/monero_c";
-const kMoneroCHash = "1ef9024e43e774ac2c2976c33bf9024549c9c61b";
+const kMoneroCHash = "64e4c987561b3aaa9ee1fbf6035a34ec8a18f654";
 
 final envProjectDir =
     File.fromUri(Platform.script).parent.parent.parent.parent.path;
