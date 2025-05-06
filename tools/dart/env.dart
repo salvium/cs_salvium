f0de860f17679d38cd2c8c06209ee3f9da10bb66
sneurlax <sneurlax@gmail.com>
2025-05-06 13:51:58 -0500

    feat(salvium_c): use salvium_c v0.9.6

--- tools/dart/env.dart
+++ tools/dart/env.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 const kSalviumCRepo = "https://github.com/cypherstack/salvium_c";
-const kSalviumCHash = "136288642fad8c76c67bd0957e654474520b5e65";
+const kSalviumCHash = "d837147a0d6d5cd658c702266b692be44e2e4657";
 
 final envProjectDir =
     File.fromUri(Platform.script).parent.parent.parent.parent.path;
