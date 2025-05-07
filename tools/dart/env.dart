85a81e3234c8a89593d896bf5710bd7dec5850c1
sneurlax <sneurlax@gmail.com>
2025-05-07 14:19:02 -0500

    fix(salvium_c): fix env.dart variable names and dir structure

--- tools/dart/env.dart
+++ tools/dart/env.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-const kSalviumCRepo = "https://github.com/cypherstack/salvium_c";
-const kSalviumCHash = "d837147a0d6d5cd658c702266b692be44e2e4657";
+const kMoneroCRepo = "https://github.com/cypherstack/salvium_c";
+const kMoneroCHash = "d837147a0d6d5cd658c702266b692be44e2e4657";
 
 final envProjectDir =
     File.fromUri(Platform.script).parent.parent.parent.parent.path;
