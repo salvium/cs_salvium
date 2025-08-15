af3e4b8c7d2181173c3de4b610d91e94e825580b
Some Random Crypto Guy <somerandomcryptoguy@protonmail.com>
2025-08-15 09:21:59 +0100

    switched from cypherstack:salvium_c to salvium:monero_c lib

--- tools/dart/env.dart
+++ tools/dart/env.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
-const kMoneroCRepo = "https://github.com/cypherstack/salvium_c";
-const kMoneroCHash = "3201cf95595e41a2243bc52713abf1bf7a4317ce";
+const kMoneroCRepo = "https://github.com/salvium/monero_c";
+const kMoneroCHash = "ac72e852a5d99429181f344ccae5b187125d8935";
 
 final envProjectDir =
     File.fromUri(Platform.script).parent.parent.parent.parent.path;
 
 String get envToolsDir => "$envProjectDir${Platform.pathSeparator}tools";
 String get envBuildDir => "$envProjectDir${Platform.pathSeparator}build";
-String get envMoneroCDir => "$envBuildDir${Platform.pathSeparator}salvium_c";
+String get envMoneroCDir => "$envBuildDir${Platform.pathSeparator}monero_c";
 String get envOutputsDir =>
     "$envProjectDir${Platform.pathSeparator}built_outputs";
