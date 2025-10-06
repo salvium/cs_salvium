bc79971363e9bee6f0158ee6ee54473f8b8188ac
Some Random Crypto Guy <somerandomcryptoguy@protonmail.com>
2025-10-06 12:28:20 +0100

    bumped monero_c version to support latest salvium version

--- tools/dart/env.dart
+++ tools/dart/env.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 const kMoneroCRepo = "https://github.com/salvium/monero_c";
-const kMoneroCHash = "606b38f999bea96b302304267206c6540ae4582f";
+const kMoneroCHash = "23f475eb6d3d204ab5f5bf1f63ae52281b0a96a6";
 
 final envProjectDir =
     File.fromUri(Platform.script).parent.parent.parent.parent.path;
